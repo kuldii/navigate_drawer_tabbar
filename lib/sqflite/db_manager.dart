37bd5560dccb98dd3e05715f7174e55a58eadc7e
kuldii <sandikha.rahardi@gmail.com>
2022-06-24 22:11:13 +0700

    Delete unused import

--- lib/sqflite/db_manager.dart
+++ lib/sqflite/db_manager.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:sqflite/sqlite_api.dart';
 
 class DatabaseManager {
   DatabaseManager._privateConstructor();
