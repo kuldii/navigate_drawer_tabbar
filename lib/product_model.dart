dcfdcf8c439e4697f4ef994c6491f68ddb8236a5
kuldii <sandikha.rahardi@gmail.com>
2022-06-24 07:44:42 +0700

    Menggunakan data asset json

--- lib/product_model.dart
+++ lib/product_model.dart
@@ -1,24 +1,34 @@
 class Product {
   int? id;
-  String? name;
-  String? desc;
+  String? title;
+  String? description;
+  String? category;
   int? price;
 
-  Product({this.id, this.name, this.desc, this.price});
+  Product({this.id, this.title, this.description, this.category, this.price});
 
   Product.fromJson(Map<String, dynamic> json) {
     id = json['id'];
-    name = json['name'];
-    desc = json['desc'];
+    title = json['title'];
+    description = json['description'];
+    category = json['category'];
     price = json['price'];
   }
 
   Map<String, dynamic> toJson() {
     final data = <String, dynamic>{};
     data['id'] = id;
-    data['name'] = name;
-    data['desc'] = desc;
+    data['title'] = title;
+    data['description'] = description;
+    data['category'] = category;
     data['price'] = price;
     return data;
+  }
+
+  static List<Product> fromJsonList(List? data) {
+    if (data == null) {
+      return [];
+    }
+    return data.map((e) => Product.fromJson(e)).toList();
   }
 }
